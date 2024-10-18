0bf7943f70b0e76c51fbe27f714d1b861d5b3ca1
ajaya <ajayasauden.05@gmail.com>
2024-10-18 11:53:20 +0545

    some input fields are added

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+function chooseWay(event){
+  const buttons= document.querySelectorAll(".radio-button");
+ 
+  buttons.forEach(button=>{
+    if(button == event.target){
+        button.classList.add('active')
+    }
+    else{
+        button.classList.remove('active')
+    }
+  })
+  if(value == )
+}
